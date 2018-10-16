396b559110db7cb12c62b6fc00250a76db22ff55
Calvin Chan <calvin.chan.h@gmail.com>
2018-10-16 14:25:51 -0400

    Added fix for FormattedMessages rule

--- tests/lib/rules/no-unwrapped-jsx-text.js
+++ tests/lib/rules/no-unwrapped-jsx-text.js
@@ -2,44 +2,46 @@
  * @fileoverview No JSXText elements without FormattedMessage
  * @author Calvin
  */
-"use strict";
+'use strict'
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/no-unwrapped-jsx-text"),
-
-    RuleTester = require("eslint").RuleTester;
-
+var rule = require('../../../lib/rules/no-unwrapped-jsx-text'),
+  RuleTester = require('eslint').RuleTester
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester({
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  }
-});
-ruleTester.run("no-unwrapped-jsx-text", rule, {
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+})
+ruleTester.run('no-unwrapped-jsx-text', rule, {
+  valid: [
+    {
+      code: 'const a = <FormattedMessage defaultMessage="asdf" />',
+    },
+    // give me some code that won't trigger a warning
+  ],
 
-    valid: [
-
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
+  invalid: [
+    {
+      code: 'const a = <div>asdf</div>',
+      errors: [
         {
-            code: "const a = <div>asdf</div>",
-            errors: [{
-                message: "JSXText should not be rendered without FormattedMessage",
-                type: "JSXElement"
-            }]
-        }
-    ]
-});
+          message: 'JSXText should not be rendered without FormattedMessage',
+          type: 'JSXElement',
+        },
+      ],
+      output: 'const a = <div><FormattedMessage defaultMessage="asdf" /></div>',
+    },
+  ],
+})
